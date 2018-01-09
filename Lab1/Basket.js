740995862c53281a414029699f3ac4124470daa2
AnastasiaKotseruba <34868876+AnastasiaKotseruba@users.noreply.github.com>
2018-01-09 15:26:23 +0200

    Update Basket.js

--- Lab1/Basket.js
+++ Lab1/Basket.js
@@ -9,26 +9,64 @@ var prodcuts = [{
 }];
 
 class ProductLineItem {
-	constructor(product) {
-		//implement
-	}
-}
+  	
+  	constructor(product) {
 
+ 		this.amount = 1;
+ 		this.product = product;
+  	}
+ 	
+ 	  getAmount () {
+ 		return this.amount;
+ 	}
+ 	 setAmount(quantity) {
+ 		this.amount = quantity;
+ 	}
+ 	 getProduct() {
+ 		return this.product;
+ 	}
+ 	
+ 	
+  }
+ var basket = (function(){
+ 
+ 	var a = []
+  	return {
+  		addProduct : function(productID){
+ 		
+ 			if(a[productID] == null) {
+ 				if ( prodcuts[productID].inventory-1 >= 0) {
+ 					a[productID] = new ProductLineItem(prodcuts[productID]);
+ 					a[productID].getProduct().inventory--;
+ 				} else {
+ 					alert("Немає продуктів на складі!");
+ 				}
+ 			} else {
+ 				alert("Ви вже вибрали цей продукт");
+ 			}
+ 				
+  		},
+  		removeProduct : function(productID){
 
-var basket = (function(){
-	//implement
-	return {
-		addProduct : function(productID){
-			//implement
+ 			prodcuts[productID].inventory = prodcuts[productID].inventory + a[productID].getAmount();
+ 			delete a[productID];
+  		},
+  		updateProductQuantity : function(productID, quantity) {
+
+ 			if ( a[productID].getProduct().inventory-quantity +  a[productID].getAmount() >= 0) {
+ 				a[productID].getProduct().inventory = a[productID].getProduct().inventory - quantity + a[productID].getAmount();
+ 				a[productID].setAmount(quantity);
+ 			} else 
+ 				alert("Немає продуктів на складі!");
 		},
-		removeProduct : function(productID){
-			//implement
-		},
-		updateProductQuantity : function(productID, quantity) {
-			//implement
-		},
-		getTotalPrice : function(){
-			//implement
-		}
-	}
-})();
+ 		
+  		getTotalPrice : function(){
+ 		
+ 			var totalPrice = 0;
+ 			a.forEach(function(item, i, arr) {
+ 			     totalPrice += item.getProduct().price * item.getAmount();
+ 			});
+ 		alert(totalPrice);
+  		}
+  	}
+ })(); 
